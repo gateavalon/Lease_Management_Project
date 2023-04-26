1ae9637c379f2762df34532a20bafb8bc1c93869
gl <gateavalonliu@gmail.com>
2023-04-26 22:45:35 +1000

    LMS: add side nav bar

---
+++ src/constants/index.tsx
@@ -0,0 +1,20 @@
+import { IconType } from "react-icons";
+import {
+  FiHome,
+  FiTrendingUp,
+  FiCompass,
+  FiStar,
+  FiSettings,
+} from "react-icons/fi";
+
+export interface LinkItemProps {
+  name: string;
+  icon: IconType;
+}
+export const LinkItems: Array<LinkItemProps> = [
+  { name: "Dashboard", icon: FiHome },
+  { name: "Trending", icon: FiTrendingUp },
+  { name: "Explore", icon: FiCompass },
+  { name: "Favourites", icon: FiStar },
+  { name: "Settings", icon: FiSettings },
+];
